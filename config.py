ed963014462c3bbc35e064a6bc908032d02c5622
Philipenko Vladimir <vovaf709@yandex.ru>
2021-09-18 17:25:11 +0300

    Baseline changes

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+# API tokens
+crypto_api_token = os.environ['CRYPTO_TOKEN']
+bot_api_token = os.environ['BOT_TOKEN']
+
+# URL-s
+crypto_api_url = 'https://min-api.cryptocompare.com/data/price?'
--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-import aiohttp
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor 
+from aiogram.utils import executor
 
-import os
+from config import bot_api_token
+from models import CryptoApi  # noqa TODO: fix flake8 configuration
 
 
-bot = Bot(os.environ['BOT_TOKEN'])
+bot = Bot(bot_api_token)
 dispatcher = Dispatcher(bot)
+crypto_api = CryptoApi()
+
 
 @dispatcher.message_handler(commands=['start', 'help'])
 async def send_greeting(message: types.Message):
     """Sends greeting to user"""
-    await bot.send_message(message.chat.id, 'Здарова! Совет дня: инвестируй в крипту')
+    await bot.send_message(
+        message.chat.id, 'Здарова! Совет дня: инвестируй в крипту'
+    )
+
+
+@dispatcher.message_handler(commands=['crypt'])
+async def send_crypt(message: types.Message):
+    """Sends crypt price"""
+    crypt_price = await crypto_api.get_price()
+    await bot.send_message(message.chat.id, crypt_price)
+
 
 if __name__ == '__main__':
     executor.start_polling(dispatcher)
