a8cf1e62510da50e964c0bda74a3880901c6baae
Philipenko Vladimir <vovaf709@yandex.ru>
2021-09-25 00:14:03 +0300

    Remove dead '/gl' handler, fix dependecies

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import logging
 import os
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
-from aiogram.utils.executor import start_webhook
-from aiogram.utils.markdown import text, bold, italic, code, pre
 from aiogram.types import ParseMode
 from aiogram.utils.emoji import emojize
+from aiogram.utils.executor import start_webhook
+from aiogram.utils.markdown import bold, italic
 
 from api_tokens import bot_api_token
 from models.crypto import (
@@ -13,7 +13,6 @@ from models.crypto import (
     CryptoApi,
 )
 from models.stocks import StocksApi
-
 
 
 WEBHOOK_PATH = ""  # Part of WEBHOOK_URL
@@ -37,11 +36,12 @@ def format_output(data):
             message += format_output(val)
     else:
         if '%' == str(data)[-1]:
-            emoji_color = (emoji_red if float(data[:-1]) < 0 else emoji_green)
+            emoji_color = emoji_red if float(data[:-1]) < 0 else emoji_green
         else:
             emoji_color = ""
         message += f'{emoji_point} {bold(data)} {emoji_color}'
     return emojize(message)
+
 
 @dispatcher.message_handler(commands=['start', 'help'])
 async def send_greeting(message: types.Message):
@@ -67,11 +67,11 @@ async def send_stock(message: types.Message):
     await bot.send_message(message.chat.id, format_output(stock_price))
 
 
-@dispatcher.message_handler(commands=['gl'])
-async def send_crypto_gainers_losers(message: types.Message):
-    """Sends stock price"""
-    gainers_losers = await coin_market_api.gainers_losers()
-    await bot.send_message(message.chat.id, gainers_losers)
+# @dispatcher.message_handler(commands=['gl'])
+# async def send_crypto_gainers_losers(message: types.Message):
+#    """Sends stock price"""
+#    gainers_losers = await coin_market_api.gainers_losers()
+#    await bot.send_message(message.chat.id, gainers_losers)
 
 
 @dispatcher.message_handler(commands=['coins'])
@@ -95,7 +95,7 @@ async def send_few_coins_price(message: types.Message):
 async def on_startup(dp):
     logging.warning('Starting up..')
 
-    await bot.set_webhook(WEBHOOK_URL)
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
     # insert code here to run it after start
     logging.warning('Lets go!')
 
@@ -113,23 +113,27 @@ async def on_shutdown(dp):
 
 if __name__ == '__main__':
     if 'DYNO' in os.environ:
-        #debug = False
-        PROJECT_NAME = os.getenv('PROJECT_NAME', 'aiogram-example')  # Set it as you've set TOKEN env var
+        # debug = False
+        PROJECT_NAME = os.getenv(
+            'PROJECT_NAME', 'aiogram-example'
+        )  # Set it as you've set TOKEN env var
 
-        # WEBHOOK_HOST = f'https://{PROJECT_NAME}.herokuapp.com/'  # Enter here your link from Heroku project settings
+        # Enter here your link from Heroku project settings
+        # WEBHOOK_HOST = f'https://{PROJECT_NAME}.herokuapp.com/'
 
+        # Domain name or IP addres which your bot is located.
+        WEBHOOK_URL = f'https://{PROJECT_NAME}.herokuapp.com/'
 
-        WEBHOOK_URL = f'https://{PROJECT_NAME}.herokuapp.com/'  # Domain name or IP addres which your bot is located.
-       
         # webserver settings
-        WEBAPP_HOST = '0.0.0.0'  # or ip 
+        WEBAPP_HOST = '0.0.0.0'  # or ip
         WEBAPP_PORT = os.getenv('PORT')
     else:
-        #debug = True
-        WEBHOOK_URL = 'ngrok'  # Domain name or IP addres which your bot is located.
-       
+        # debug = True
+        # Domain name or IP addres which your bot is located.
+        WEBHOOK_URL = 'https://e4b8-95-25-66-88.ngrok.io'
+
         # webserver settings
-        WEBAPP_HOST = '0.0.0.0'  # or ip 
+        WEBAPP_HOST = '0.0.0.0'  # or ip
         WEBAPP_PORT = 5000
     start_webhook(
         dispatcher=dispatcher,
