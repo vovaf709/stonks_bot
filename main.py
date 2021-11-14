48dac6fb351606c556cd46db59398a5409fafa51
artemensor <artem2000200@mail.ru>
2021-11-14 11:08:33 +0300

    'fixes'

--- main.py
+++ main.py
@@ -150,3 +150,6 @@ if __name__ == '__main__':
     # web.run_app(app, host=WEBAPP_HOST, port=WEBAPP_PORT)
     # Heroku stores port you have to listen in your ap
     # web.run_app(app, host='0.0.0.0', port=os.getenv('PORT'))
+
+
+    
